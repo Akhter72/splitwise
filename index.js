d628f0e83489c1c5116089d1276d998b05af8458
AkhterI22202 <akhterhussain.dar@ideas2it.com>
2023-05-16 20:47:20 +0530

    Test

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+import HomepageDrawer from './src/components/HomepageDrawer';
+import Calender from './src/components/Calender';
+
+AppRegistry.registerComponent(appName, () => App);
